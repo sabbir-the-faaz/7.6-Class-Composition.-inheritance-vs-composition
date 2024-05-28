a3f820c7bf1da0bb5025f607ca1bc82d8c9265c7
Md. Sabbir Rahman Akash <161277809+the-faaz@users.noreply.github.com>
2024-05-28 07:37:50 +0600

    Add files via upload

---
+++ 7.6 Class Composition. inheritance vs composition.py.py
@@ -0,0 +1,16 @@
+class Engine:
+  def __init__(se)-> None:
+    pass
+  
+  def start(Self):
+    return "Engine started "
+  
+class driver:
+  def __init__(self) -> None:
+    pass
+  
+#it means car "has an" engine 
+class car:
+  def __init__(self) -> None:
+    self.engine =Engine()
+    self.driver = driver()
